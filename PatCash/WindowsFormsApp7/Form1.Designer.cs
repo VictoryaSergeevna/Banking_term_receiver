bce0f332ce6cde064f34133977f29948d20045db
VictoryaSergeevna <vikusyachk@gmail.com>
2020-05-29 16:14:48 +0300

    it's all

---
+++ PatCash/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblDiameter = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.textBoxDiametr = new System.Windows.Forms.TextBox();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDiameter
+            // 
+            this.lblDiameter.AutoSize = true;
+            this.lblDiameter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDiameter.Location = new System.Drawing.Point(27, 109);
+            this.lblDiameter.Name = "lblDiameter";
+            this.lblDiameter.Size = new System.Drawing.Size(103, 24);
+            this.lblDiameter.TabIndex = 0;
+            this.lblDiameter.Text = "Диаметр:";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblWeight.Location = new System.Drawing.Point(27, 188);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(46, 24);
+            this.lblWeight.TabIndex = 1;
+            this.lblWeight.Text = "Вес";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSum.Location = new System.Drawing.Point(27, 272);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(73, 24);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "Сумма";
+            // 
+            // textBoxDiametr
+            // 
+            this.textBoxDiametr.Location = new System.Drawing.Point(233, 109);
+            this.textBoxDiametr.Name = "textBoxDiametr";
+            this.textBoxDiametr.Size = new System.Drawing.Size(166, 20);
+            this.textBoxDiametr.TabIndex = 3;
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Location = new System.Drawing.Point(233, 272);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(166, 20);
+            this.textBoxSum.TabIndex = 4;
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(233, 188);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(166, 20);
+            this.textBoxWeight.TabIndex = 5;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCheck.Location = new System.Drawing.Point(64, 344);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(113, 31);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(308, 344);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 28);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClose.BackgroundImage")));
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Location = new System.Drawing.Point(464, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 32);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClearAll.Location = new System.Drawing.Point(165, 410);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(151, 28);
+            this.btnClearAll.TabIndex = 8;
+            this.btnClearAll.Text = "Очистить все";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 450);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxSum);
+            this.Controls.Add(this.textBoxDiametr);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblDiameter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDiameter;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.TextBox textBoxDiametr;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClearAll;
+    }
+}
+
---
+++ PatCash/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        Penny penny = new Penny();
+        Client client = new Client();
+        Handler fivePenny = new FivePennyHandler();
+        Handler tenPenny = new TenPennyHandler();
+        Handler twentyFivePenny = new TwentyFivePennyHandler();
+        Handler fiftyPenny = new FiftyPennyHandler();
+        Handler oneGrnPenny = new OneGrnPennyHandler();
+        ImageList list = null;
+        ToolBar toolBar = null;
+        public Form1()
+        {
+            InitializeComponent();
+            fivePenny.Successor = tenPenny;
+            tenPenny.Successor = twentyFivePenny;
+            twentyFivePenny.Successor = fiftyPenny;
+            fiftyPenny.Successor = oneGrnPenny;
+            list = new ImageList();
+            list.ImageSize = new Size(50, 50);
+            list.Images.Add(new Bitmap("5.bmp"));
+            list.Images.Add(new Bitmap("10.bmp"));
+            list.Images.Add(new Bitmap("25.bmp"));
+            list.Images.Add(new Bitmap("50.bmp"));
+            list.Images.Add(new Bitmap("1.bmp"));
+            toolBar = new ToolBar();
+            toolBar.Appearance = ToolBarAppearance.Flat;
+            toolBar.BorderStyle = BorderStyle.Fixed3D;
+            toolBar.ImageList = list;
+            ToolBarButton toolBarButton1 = new ToolBarButton();
+            toolBarButton1.Tag = "5";
+            ToolBarButton toolBarButton2 = new ToolBarButton();
+            toolBarButton2.Tag = "10";
+            ToolBarButton toolBarButton3 = new ToolBarButton();
+            toolBarButton3.Tag = "25";
+            ToolBarButton toolBarButton4 = new ToolBarButton();
+            toolBarButton4.Tag = "50";
+            ToolBarButton toolBarButton5 = new ToolBarButton();
+            toolBarButton5.Tag = "1";      
+            
+
+            toolBarButton1.ImageIndex = 0;
+            toolBarButton2.ImageIndex = 1;
+            toolBarButton3.ImageIndex = 2;
+            toolBarButton4.ImageIndex = 3;
+            toolBarButton5.ImageIndex = 4;
+           
+
+            toolBar.Buttons.Add(toolBarButton1);
+            toolBar.Buttons.Add(toolBarButton2);
+            toolBar.Buttons.Add(toolBarButton3);
+            toolBar.Buttons.Add(toolBarButton4);
+            toolBar.Buttons.Add(toolBarButton5);          
+            this.Controls.Add(toolBar);
+            toolBar.ButtonClick += ToolBar_ButtonClick;
+
+        }
+
+        private void ToolBar_ButtonClick(object sender, ToolBarButtonClickEventArgs e)
+        {
+            switch (e.Button.Tag)
+            {
+                case "5":
+                    MessageBox.Show("Диаметр: 24 / Вес: 4", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "10":
+                    MessageBox.Show("Диаметр: 16 / Вес: 2", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "25":
+                    MessageBox.Show("Диаметр: 21 / Вес: 3", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "50":
+                    MessageBox.Show("Диаметр: 23 / Вес: 4", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "1":
+                    MessageBox.Show("Диаметр: 26 / Вес: 7", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            penny.Weight = Convert.ToInt32(textBoxDiametr.Text);
+            penny.Diameter = Convert.ToInt32(textBoxWeight.Text);
+            fivePenny.HandleRequest(penny, client);
+            if (client.Sum >= 100)
+            {
+                textBoxSum.Text = (client.Sum / 100).ToString() + " грн." + (client.Sum % 100).ToString() + " коп";
+            }
+            else
+            {
+                textBoxSum.Text = client.Sum.ToString() + " коп";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxDiametr.Clear();
+            textBoxWeight.Clear();
+        }
+
+       
+
+
+        public class Penny
+        {
+            public int Weight { get; set; }
+            public int Diameter { get; set; }
+        }
+
+        abstract class Handler
+        {
+            public Handler Successor { get; set; }
+            public abstract void HandleRequest(Penny penny, Client client);
+        }
+
+        class FivePennyHandler : Handler
+        {
+            public override void HandleRequest(Penny penny, Client client)
+            {
+                if (penny.Weight == 24 && penny.Diameter == 4)
+                {
+                    client.Sum += 5;
+                }
+                else if (Successor != null)
+                {
+                    Successor.HandleRequest(penny, client);
+                }
+            }
+        }
+        class TenPennyHandler : Handler
+        {
+            public override void HandleRequest(Penny penny, Client client)
+            {
+                if (penny.Weight == 16 && penny.Diameter == 2)
+                {
+                    client.Sum += 10;
+                }
+                else if (Successor != null)
+                {
+                    Successor.HandleRequest(penny, client);
+                }
+            }
+        }
+        class TwentyFivePennyHandler : Handler
+        {
+            public override void HandleRequest(Penny penny, Client client)
+            {
+                if (penny.Weight == 21 && penny.Diameter == 3)
+                {
+                    client.Sum += 25;
+                }
+                else if (Successor != null)
+                {
+                    Successor.HandleRequest(penny, client);
+                }
+            }
+        }
+        class FiftyPennyHandler : Handler
+        {
+            public override void HandleRequest(Penny penny, Client client)
+            {
+                if (penny.Weight == 23 && penny.Diameter == 4)
+                {
+                    client.Sum += 50;
+                }
+                else if (Successor != null)
+                {
+                    Successor.HandleRequest(penny, client);
+                }
+            }
+        }
+        class OneGrnPennyHandler : Handler
+        {
+            public override void HandleRequest(Penny penny, Client client)
+            {
+                if (penny.Weight == 26 && penny.Diameter == 7)
+                {
+                    client.Sum += 100;
+                }
+                else if (Successor != null)
+                {
+                    Successor.HandleRequest(penny, client);
+                }
+                else
+                {
+                    MessageBox.Show("Введите правильный вес и диаметр монеты!", "Монеты", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        class Client
+        {
+            public int Sum { get; set; }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            textBoxDiametr.Clear();
+            textBoxWeight.Clear();
+            textBoxSum.Clear();
+        }
+    }
+}
+
